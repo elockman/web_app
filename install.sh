2f56e260fff6333be94fb23eff33009cd01d1103
Eric Lockman <eric@xealstudios.com>
2021-09-30 16:16:57 -0600

    update Makefile

---
+++ install.sh
@@ -0,0 +1,24 @@
+mkdir /usr/local
+mkdir /usr/local/lib
+git clone https://github.com/babelouest/orcania.git
+cd orcania/
+make && sudo make install
+cd ..
+
+git clone https://github.com/babelouest/yder.git
+cd yder/src
+make Y_DISABLE_JOURNALD=1
+sudo make install
+
+cd ../..
+git clone https://github.com/babelouest/ulfius.git
+
+cd ulfius/
+make CURLFLAG=1 GNUTLSFLAG=1 JANSSONFLAG=1
+sudo make install
+
+cd ..
+git clone https://github.com/elockman/web_app.git
+mv web_app/ ./ulfius/example_programs/
+cd ./ulfius/example_programs/web_app/
+make
